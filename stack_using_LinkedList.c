8d8af3fed5bbe809a2ddea5f4b0c8b3b4624ca87
vignesh1000 <70900581+vignesh1000@users.noreply.github.com>
2021-09-12 12:56:01 +0530

    Create stack_using_LinkedList.c
    Stack implementation using linked list

---
+++ stack_using_LinkedList.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+}*head;
+
+
+void push(int iData)
+{
+	struct node *temp;
+
+	temp = (struct node*)malloc(sizeof(struct node));
+	temp->data = iData;
+
+	temp->next = head;
+	head = temp;
+}
+
+struct node* pop(void)
+{
+	struct node *temp = head;
+	
+	head = head->next;
+
+	return temp;
+}
+
+void main()
+{
+	struct node* tmp;
+	head = (struct node*)malloc(sizeof(struct node));
+
+	push(2);
+	push(3);
+	push(4);
+	push(5);
+	push(6);
+
+	tmp = pop();
+	printf("elements : %d\n",tmp->data);
+	tmp = pop();
+	printf("elements : %d\n",tmp->data);
+	tmp = pop();
+	printf("elements : %d\n",tmp->data);
+	tmp = pop();
+	printf("elements : %d\n",tmp->data);
+		tmp = pop();
+	printf("elements : %d\n",tmp->data);
+
+	while(1);
+}
